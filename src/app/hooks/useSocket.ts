3520aa696a2ff2f60d72b4d779ebf795a05e874e
angar2 <uky00832@gmail.com>
2024-08-27 03:03:30 +0900

    feat: add room exit functionality for chat

--- src/app/hooks/useSocket.ts
+++ src/app/hooks/useSocket.ts
@@ -47,7 +47,18 @@ export default function useSocket(
 
     setSocket(socket);
 
+    const handleBeforeUnload = () => {
+      // 방 퇴장
+      socket.emit('leave', { roomId });
+    };
+
+    // 페이지 이벤트 설정(방 퇴장)
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
     return () => {
+      // 페이지 이벤트 제거
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+
       // 소켓 연결 해제
       socket.disconnect();
     };
