bbef9491abe5fb87c4650f361a3f2dbbbe177fa9
angar2 <uky00832@gmail.com>
2024-08-27 23:47:34 +0900

    feat: re-enable socket leave event

--- src/app/hooks/useSocket.ts
+++ src/app/hooks/useSocket.ts
@@ -55,17 +55,17 @@ export default function useSocket(
 
     setSocket(socket);
 
-    // const handleBeforeUnload = () => {
-    //   // 방 퇴장
-    //   socket.emit('leave', { roomId });
-    // };
+    const handleBeforeUnload = () => {
+      // 방 퇴장
+      socket.emit('leave', { roomId });
+    };
 
-    // // 페이지 이벤트 설정(방 퇴장)
-    // window.addEventListener('beforeunload', handleBeforeUnload);
+    // 페이지 이벤트 설정(방 퇴장)
+    window.addEventListener('beforeunload', handleBeforeUnload);
 
     return () => {
-      // // 페이지 이벤트 제거
-      // window.removeEventListener('beforeunload', handleBeforeUnload);
+      // 페이지 이벤트 제거
+      window.removeEventListener('beforeunload', handleBeforeUnload);
 
       // 소켓 연결 해제
       socket.disconnect();
