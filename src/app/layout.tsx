030c431bd4fe3cebe4f04d92c112618fb681670b
angar2 <uky00832@gmail.com>
2024-09-16 02:42:44 +0900

    chore: configure favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/images/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/assets/images/favicon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/images/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/images/favicon/manifest.json" />
+        <link
+          rel="browserconfig"
+          href="/assets/images/favicon/browserconfig.xml"
+        />
+        <meta name="apple-mobile-web-app-title" content="Cola Chat" />
+        <meta name="application-name" content="Cola Chat" />
+        <meta name="msapplication-TileColor" content="#F0EFDB" />
+        <meta
+          name="msapplication-TileImage"
+          content="/assets/images/favicon/ms-icon-144x144.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen bg-b">
           <Header />
